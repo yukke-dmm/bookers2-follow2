3c778b753c7ce240ec095d80fa4d0bca7840dd53
森田悠介 <yusuke@moritayuusukenoMacBook-Air.local>
2020-06-18 15:52:02 +0900

    フォローフォロワー作り直し

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books,only: [:new, :create, :index, :show, :edit, :update, :delete] do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :book_comments,only: [:create, :destroy]
+  end
+
+  root 'home#top'
+  get 'home/about'
+end
